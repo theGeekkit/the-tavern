8d7215850f956df9db6a9378219d0b6e47c6156c
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2023-02-13 19:03:43 -0600

    generated comment scaffold 2-13-23
    Co-authored-by: kylesteinmann <kylesteinmann@users.noreply.github.com>
    Co-authored-by: chrisHembree <chrisHembree@users.noreply.github.com>

--- db/migrate/20230214005303_create_comments.rb
+++ db/migrate/20230214005303_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.belongs_to :user, null: false, foreign_key: true
+      t.string :content
       t.belongs_to :post, null: false, foreign_key: true
-      t.string :comment
+      t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_10_003648) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_14_005303) do
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "post"
     t.datetime "created_at", null: false
@@ -25,5 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_10_003648) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
