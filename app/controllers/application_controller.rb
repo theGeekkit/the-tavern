3b474c966d291a552814afc2cc06aa65dcff8d2c
theGeekkit <sonofgondolin16@gmail.com>
2023-02-18 07:49:55 -0600

    2-18-23

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    def require_user
+        # if user is a guest, redirect to log in page
+        if guest?
+            flash[:notice] = "You must log in first."
+            redirect_to login_path
+        end
+    end
+
+    def guest? 
+        !helper.loggedin?
+    end
 end
