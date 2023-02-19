59ab9f12d49840296fa49b7883d56ffd57150702
kylesteinmann <105984447+kylesteinmann@users.noreply.github.com>
2023-02-18 21:05:29 -0600

    fixing mistake

--- config/initializers/cors.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'localhost:4400'
-      resource '*', headers: :any, methods: [:get, :post]
-    end
-  end
